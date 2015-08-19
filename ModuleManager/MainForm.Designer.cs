332446a6ecfcf8b7cd17b811f79d596776bc0e98
Scott Kauker <swkauker@yahoo.com>
2015-08-19 04:44:30 -0700

    Show additional information

--- ModuleManager/MainForm.Designer.cs
+++ ModuleManager/MainForm.Designer.cs
@@ -37,22 +37,22 @@
             this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.moduleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.listAllModules = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.openProjectDialog = new System.Windows.Forms.OpenFileDialog();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.panel1 = new System.Windows.Forms.Panel();
+            this.textAdditionalDependencies = new System.Windows.Forms.TextBox();
+            this.labelModuleLoadingPhase = new System.Windows.Forms.Label();
+            this.labelModuleType = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelModulePath = new System.Windows.Forms.Label();
+            this.labelRealm = new System.Windows.Forms.Label();
+            this.labelModuleName = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.labelModuleName = new System.Windows.Forms.Label();
-            this.labelRealm = new System.Windows.Forms.Label();
-            this.labelModulePath = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.labelModuleType = new System.Windows.Forms.Label();
-            this.labelModuleLoadingPhase = new System.Windows.Forms.Label();
-            this.textAdditionalDependencies = new System.Windows.Forms.TextBox();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
@@ -126,11 +126,16 @@
             this.listAllModules.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
             this.listAllModules.Location = new System.Drawing.Point(0, 0);
             this.listAllModules.Name = "listAllModules";
-            this.listAllModules.Size = new System.Drawing.Size(250, 200);
+            this.listAllModules.Size = new System.Drawing.Size(250, 209);
             this.listAllModules.TabIndex = 1;
             this.listAllModules.UseCompatibleStateImageBehavior = false;
             this.listAllModules.View = System.Windows.Forms.View.Details;
             this.listAllModules.SelectedIndexChanged += new System.EventHandler(this.listAllModules_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            this.columnHeader1.Width = 196;
             // 
             // openProjectDialog
             // 
@@ -151,14 +156,9 @@
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.panel1);
-            this.splitContainer1.Size = new System.Drawing.Size(480, 200);
+            this.splitContainer1.Size = new System.Drawing.Size(480, 209);
             this.splitContainer1.SplitterDistance = 250;
             this.splitContainer1.TabIndex = 2;
-            // 
-            // columnHeader1
-            // 
-            this.columnHeader1.Text = "Name";
-            this.columnHeader1.Width = 196;
             // 
             // panel1
             // 
@@ -177,8 +177,109 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(226, 200);
+            this.panel1.Size = new System.Drawing.Size(226, 209);
             this.panel1.TabIndex = 0;
+            // 
+            // textAdditionalDependencies
+            // 
+            this.textAdditionalDependencies.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textAdditionalDependencies.Location = new System.Drawing.Point(15, 86);
+            this.textAdditionalDependencies.Multiline = true;
+            this.textAdditionalDependencies.Name = "textAdditionalDependencies";
+            this.textAdditionalDependencies.ReadOnly = true;
+            this.textAdditionalDependencies.Size = new System.Drawing.Size(199, 69);
+            this.textAdditionalDependencies.TabIndex = 14;
+            // 
+            // labelModuleLoadingPhase
+            // 
+            this.labelModuleLoadingPhase.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelModuleLoadingPhase.AutoEllipsis = true;
+            this.labelModuleLoadingPhase.Location = new System.Drawing.Point(96, 48);
+            this.labelModuleLoadingPhase.Name = "labelModuleLoadingPhase";
+            this.labelModuleLoadingPhase.Size = new System.Drawing.Size(118, 13);
+            this.labelModuleLoadingPhase.TabIndex = 13;
+            this.labelModuleLoadingPhase.Text = "None";
+            this.labelModuleLoadingPhase.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // labelModuleType
+            // 
+            this.labelModuleType.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelModuleType.AutoEllipsis = true;
+            this.labelModuleType.Location = new System.Drawing.Point(57, 35);
+            this.labelModuleType.Name = "labelModuleType";
+            this.labelModuleType.Size = new System.Drawing.Size(157, 13);
+            this.labelModuleType.TabIndex = 12;
+            this.labelModuleType.Text = "None";
+            this.labelModuleType.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 70);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(148, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Additional Dependencies";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 48);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Load Phase:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 35);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Type:";
+            // 
+            // labelModulePath
+            // 
+            this.labelModulePath.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelModulePath.AutoEllipsis = true;
+            this.labelModulePath.Location = new System.Drawing.Point(15, 171);
+            this.labelModulePath.Name = "labelModulePath";
+            this.labelModulePath.Size = new System.Drawing.Size(199, 30);
+            this.labelModulePath.TabIndex = 8;
+            this.labelModulePath.Text = "None";
+            // 
+            // labelRealm
+            // 
+            this.labelRealm.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelRealm.AutoEllipsis = true;
+            this.labelRealm.Location = new System.Drawing.Point(64, 22);
+            this.labelRealm.Name = "labelRealm";
+            this.labelRealm.Size = new System.Drawing.Size(150, 13);
+            this.labelRealm.TabIndex = 7;
+            this.labelRealm.Text = "None";
+            this.labelRealm.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // labelModuleName
+            // 
+            this.labelModuleName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelModuleName.AutoEllipsis = true;
+            this.labelModuleName.Location = new System.Drawing.Point(61, 9);
+            this.labelModuleName.Name = "labelModuleName";
+            this.labelModuleName.Size = new System.Drawing.Size(153, 13);
+            this.labelModuleName.TabIndex = 6;
+            this.labelModuleName.Text = "None";
+            this.labelModuleName.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
             // label3
             // 
@@ -210,111 +311,11 @@
             this.label1.TabIndex = 3;
             this.label1.Text = "Name:";
             // 
-            // labelModuleName
-            // 
-            this.labelModuleName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.labelModuleName.AutoEllipsis = true;
-            this.labelModuleName.Location = new System.Drawing.Point(61, 9);
-            this.labelModuleName.Name = "labelModuleName";
-            this.labelModuleName.Size = new System.Drawing.Size(153, 13);
-            this.labelModuleName.TabIndex = 6;
-            this.labelModuleName.Text = "None";
-            this.labelModuleName.TextAlign = System.Drawing.ContentAlignment.TopRight;
-            // 
-            // labelRealm
-            // 
-            this.labelRealm.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.labelRealm.AutoEllipsis = true;
-            this.labelRealm.Location = new System.Drawing.Point(64, 22);
-            this.labelRealm.Name = "labelRealm";
-            this.labelRealm.Size = new System.Drawing.Size(150, 13);
-            this.labelRealm.TabIndex = 7;
-            this.labelRealm.Text = "None";
-            this.labelRealm.TextAlign = System.Drawing.ContentAlignment.TopRight;
-            // 
-            // labelModulePath
-            // 
-            this.labelModulePath.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.labelModulePath.Location = new System.Drawing.Point(15, 171);
-            this.labelModulePath.Name = "labelModulePath";
-            this.labelModulePath.Size = new System.Drawing.Size(199, 21);
-            this.labelModulePath.TabIndex = 8;
-            this.labelModulePath.Text = "None";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(12, 35);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(39, 13);
-            this.label4.TabIndex = 9;
-            this.label4.Text = "Type:";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(12, 48);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(78, 13);
-            this.label5.TabIndex = 10;
-            this.label5.Text = "Load Phase:";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(12, 70);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(148, 13);
-            this.label6.TabIndex = 11;
-            this.label6.Text = "Additional Dependencies";
-            // 
-            // labelModuleType
-            // 
-            this.labelModuleType.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.labelModuleType.AutoEllipsis = true;
-            this.labelModuleType.Location = new System.Drawing.Point(57, 35);
-            this.labelModuleType.Name = "labelModuleType";
-            this.labelModuleType.Size = new System.Drawing.Size(157, 13);
-            this.labelModuleType.TabIndex = 12;
-            this.labelModuleType.Text = "None";
-            this.labelModuleType.TextAlign = System.Drawing.ContentAlignment.TopRight;
-            // 
-            // labelModuleLoadingPhase
-            // 
-            this.labelModuleLoadingPhase.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.labelModuleLoadingPhase.AutoEllipsis = true;
-            this.labelModuleLoadingPhase.Location = new System.Drawing.Point(96, 48);
-            this.labelModuleLoadingPhase.Name = "labelModuleLoadingPhase";
-            this.labelModuleLoadingPhase.Size = new System.Drawing.Size(118, 13);
-            this.labelModuleLoadingPhase.TabIndex = 13;
-            this.labelModuleLoadingPhase.Text = "None";
-            this.labelModuleLoadingPhase.TextAlign = System.Drawing.ContentAlignment.TopRight;
-            // 
-            // textAdditionalDependencies
-            // 
-            this.textAdditionalDependencies.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.textAdditionalDependencies.Location = new System.Drawing.Point(15, 86);
-            this.textAdditionalDependencies.Multiline = true;
-            this.textAdditionalDependencies.Name = "textAdditionalDependencies";
-            this.textAdditionalDependencies.ReadOnly = true;
-            this.textAdditionalDependencies.Size = new System.Drawing.Size(199, 69);
-            this.textAdditionalDependencies.TabIndex = 14;
-            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(480, 224);
+            this.ClientSize = new System.Drawing.Size(480, 233);
             this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
